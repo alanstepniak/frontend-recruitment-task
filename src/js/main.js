b683e3388fe274e4325170311dbc0d10863fe3b2
alanstepniak <99657826+alanstepniak@users.noreply.github.com>
2022-05-21 14:04:10 +0200

    dodano funkcjonalności i poprawki

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,24 @@
 
 let btn = document.getElementsByTagName("button")[0];
 let ovl = document.getElementById("dialogOverlay");
+let x = document.getElementById("close");
+let counter = document.getElementById("counter");
+let reset = document.getElementById("reset");
+
+document.getElementById('dialogBox').addEventListener('click', function(e){
+    e.stopPropagation()
+})
+
 
 customAlert = () => {
     let dialogOverlay = document.getElementById('dialogOverlay');
     let dialogBox = document.getElementById('dialogBox');
     dialogOverlay.style.display = "block";
-    dialogBox.style.display = "block";
+    dialogBox.style.display = "inline-block";
+    let newCounterValue = parseInt(counter.innerHTML)+ 1
+    counter.innerHTML = newCounterValue
+    resetShow()
+    localStorage.setItem('counter',newCounterValue);
 }
 
 closeAlert = () => {
@@ -14,5 +26,22 @@ closeAlert = () => {
     dialogBox.style.display = "none";
 }
 
+resetShow = () => {
+    if (parseInt(counter.innerHTML) > 5) {
+        return (
+            reset.style.display = "block"
+        );
+    }
+}
+
+resetValue = () => {
+    counter.innerHTML = 0;
+    reset.style.display = "none"
+}
+
 btn.addEventListener("click", customAlert);
 ovl.addEventListener("click", closeAlert);
+x.addEventListener("click", closeAlert);
+reset.addEventListener("click",resetValue);
+
+
